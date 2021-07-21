c7a2b4d726a04aea138fb15b0f39ad437e1293ca
sojohso <87091896+sojohso@users.noreply.github.com>
2021-07-21 15:19:44 -0500

    High Score
    Added functionality to calculate and save high score within the score class.

--- 2048-Final/Classes/Game.cs
+++ 2048-Final/Classes/Game.cs
@@ -11,6 +11,7 @@ namespace _2048_Final.Classes.Game
     using Score;
     using Color;
     using Color_Output;
+    using Message; //could try to inherit functions
     class Game
     {
         private readonly int columns;
@@ -19,6 +20,7 @@ namespace _2048_Final.Classes.Game
         Board empty_spaces = new Board();
         Score score = new Score();
         Color color = new Color();
+        Message msg = new Message();
         int locked;
 
         public Game()
@@ -64,7 +66,7 @@ namespace _2048_Final.Classes.Game
 
         private void print_board() //after first use
         {
-            Console.Clear();
+            //Console.Clear();
             //will add ^ after debugging
             //Board_Colors board_colors = new Board_Colors();
             for (int i = 0; i < rows; i++)
@@ -83,14 +85,35 @@ namespace _2048_Final.Classes.Game
             return;
             //insert functionality to add colors from board colors class
         }
+        internal int check_full()
+        {
+
+            int i = 0, j;
+            int tmp1, tmp2, tmp3, tmp4;
+            int full = 0;
+
+            for (j = 0; j <= 3; j++)
+            {
+                tmp1 = board.get_board_values(i, j); //uppermost
+                tmp2 = board.get_board_values(i + 1, j); //2nd uppermost
+                tmp3 = board.get_board_values(i + 2, j); //3rd uppermost
+                tmp4 = board.get_board_values(i + 3, j); //4th uppermost
+                if (tmp1 != 0 && tmp2 != 0 && tmp3 != 0 && tmp4 != 0)
+                    full++;
+            }
+            return full;
+        }
         internal void insert_random_value()
         {
             Random random_number = new Random();
-            int iVal, iRow, iCol;
+            int iVal, iRow, iCol, full;
             iVal = random_number.Next(0, 100) < 75 ? 2 : 4;
             iRow = random_number.Next(0, 3);
             iCol = random_number.Next(0, 3);
-            while(board.get_board_values(iRow, iCol) != 0) //checks will be added to make sure it does not overflow
+            full = check_full();
+            if (full == 4)
+                return;
+            while (board.get_board_values(iRow, iCol) != 0) //checks will be added to make sure it does not overflow
             {
                 iRow = random_number.Next(0, 4);
                 iCol = random_number.Next(0, 4);
@@ -130,6 +153,7 @@ namespace _2048_Final.Classes.Game
             }
             insert_random_value();
             print_board();
+            //msg.lost_check(board, score.get_score());
             keyboard_press();
             //pass which one by <, >, ^, v into the separate class
             //return this value, then main will insert that value into the class.
@@ -151,6 +175,7 @@ namespace _2048_Final.Classes.Game
                                                          //else if tmp 1 = 0 don't alter anything
                 if (tmp1 == tmp2 && tmp1 != 0 && tmp2 != 0)
                 {
+                    Console.WriteLine("hey");
                     tmp1 = tmp1 * 2;
                     board.set_board_values(0, j, tmp1);
                     if (tmp3 == tmp4 && tmp3 != 0 && tmp4 != 0)
@@ -171,6 +196,11 @@ namespace _2048_Final.Classes.Game
                     }
 
                 }
+                else if (tmp1 != 0 && tmp2 == 0 && tmp3 == 0 && tmp1 != tmp4 && tmp4 != 0)
+                {
+                    board.set_board_values(1, j, tmp4);
+                    board.set_board_values(3, j, 0);
+                }
                 else if (tmp1 == tmp3 && tmp2 == 0 && tmp1 != 0 && tmp3 != 0)
                 {
                     tmp1 = tmp1 * 2;
@@ -182,6 +212,7 @@ namespace _2048_Final.Classes.Game
                 }
                 else if (tmp2 == tmp3 && tmp1 == 0 && tmp2 != 0 && tmp3 != 0)
                 {
+                    Console.WriteLine("hey1");
                     tmp2 = tmp2 * 2;
                     board.set_board_values(0, j, tmp2);
                     board.set_board_values(1, j, tmp4);
@@ -191,6 +222,7 @@ namespace _2048_Final.Classes.Game
                 }
                 else if (tmp2 == tmp4 && tmp1 == 0 && tmp3 == 0 && tmp2 != 0 && tmp4 != 0)
                 {
+                    Console.WriteLine("hey2");
                     tmp2 = tmp2 * 2;
                     board.set_board_values(0, j, tmp2);
                     board.set_board_values(1, j, 0);
@@ -200,23 +232,41 @@ namespace _2048_Final.Classes.Game
                 }
                 else if (tmp2 == tmp4 && tmp1 != 0 && tmp3 == 0 && tmp2 != 0)
                 {
+                    Console.WriteLine("hey3");
                     tmp2 = tmp2 * 2;
                     board.set_board_values(1, j, tmp2);
                     board.set_board_values(2, j, 0);
                     board.set_board_values(3, j, 0);
                     score.calculate_score(tmp2);
-                }
-                else if (tmp3 == tmp4 && tmp1 == 0 && tmp2 == 0)
+                } else if(tmp3 == tmp4 && tmp3 != 0 && tmp2 == 0 && tmp1 != 0 && tmp1 != tmp3)
                 {
+                    Console.WriteLine("hey4");
+                    tmp3 *= 2;
+                    board.set_board_values(1, j, tmp3);
+                    board.set_board_values(2, j, 0);
+                    board.set_board_values(3, j, 0);
+                }
+                else if (tmp3 == tmp4 && tmp1 == 0 && tmp2 == 0 && tmp3 != 0)
+                {
+                    Console.WriteLine("hey5");
                     tmp3 = tmp3 * 2;
                     board.set_board_values(0, j, tmp3);
                     board.set_board_values(1, j, 0);
                     board.set_board_values(2, j, 0);
                     board.set_board_values(3, j, 0);
                     score.calculate_score(tmp3);
+                } else if (tmp3 == tmp4 && tmp3 != 0 && tmp2 == 0 && tmp3 != tmp1 && tmp1 != 0)
+                {
+                    Console.WriteLine("hey6");
+                    tmp3 *= 2;
+                    board.set_board_values(1, j, tmp3);
+                    board.set_board_values(2, j, 0);
+                    board.set_board_values(3, j, 0);
+                    score.calculate_score(tmp3);
                 }
                 else if (tmp1 == 0 && tmp2 == 0 && tmp3 == 0 && tmp4 != 0)
                 {
+                    Console.WriteLine("hey7");
                     board.set_board_values(0, j, tmp4);
                     board.set_board_values(1, j, 0);
                     board.set_board_values(2, j, 0);
@@ -224,6 +274,7 @@ namespace _2048_Final.Classes.Game
                 }
                 else if (tmp1 != tmp2 && tmp2 != 0 && tmp2 == tmp3 && tmp1 != 0 && tmp3 != 0)
                 {
+                    Console.WriteLine("hey8");
                     tmp2 = tmp2 * 2;
                     board.set_board_values(1, j, tmp2);
                     board.set_board_values(2, j, tmp4);
@@ -232,18 +283,21 @@ namespace _2048_Final.Classes.Game
                 }
                 else if (tmp1 != tmp3 && tmp2 == 0 && tmp1 != 0 && tmp3 != tmp4)
                 {
+                    Console.WriteLine("hey9");
                     board.set_board_values(1, j, tmp3);
                     board.set_board_values(2, j, 0); //add function for block to appear
                     board.set_board_values(3, j, 0); //add function for block to appear
                 }
                 else if (tmp1 != tmp4 && tmp2 == 0 && tmp3 == 0 && tmp1 != 0 && tmp4 != 0)
                 {
+                    Console.WriteLine("hey10");
                     board.set_board_values(1, j, tmp4);
                     board.set_board_values(2, j, 0); //add function for block to appear
                     board.set_board_values(3, j, 0); //add function for block to appear
                 }
                 else if(tmp1 == 0 && tmp2 != 0 && tmp3 != 0 && tmp4 != 0 && tmp2 != tmp3 && tmp2 != tmp4 && tmp3 != tmp4)
                 {
+                    Console.WriteLine("hey11");
                     board.set_board_values(0, j, tmp2);
                     board.set_board_values(1, j, tmp3);
                     board.set_board_values(2, j, tmp4);
@@ -251,7 +305,7 @@ namespace _2048_Final.Classes.Game
                 }
                 else if (tmp1 == 0 && tmp2 != 0)
                 {
-                    Console.WriteLine("hey01");
+                    Console.WriteLine("hey12");
                     board.set_board_values(0, j, tmp2);
                     board.set_board_values(1, j, tmp3);
                     board.set_board_values(2, j, tmp4);
@@ -259,6 +313,7 @@ namespace _2048_Final.Classes.Game
                 }
                 else if (tmp1 == 0 && tmp2 == 0 && tmp3 != 0)
                 {
+                    Console.WriteLine("hey13");
                     board.set_board_values(0, j, tmp3);
                     board.set_board_values(1, j, tmp4);
                     board.set_board_values(2, j, 0);
@@ -266,16 +321,27 @@ namespace _2048_Final.Classes.Game
                 }
                 else if (tmp2 == 0 && tmp2 == tmp3 && tmp2 == tmp1 && tmp4 != 0)
                 {
+                    Console.WriteLine("hey14");
                     board.set_board_values(0, j, tmp4);
                     board.set_board_values(3, j, 0); //add function for block to appear
                 }
                 else if(tmp3 == tmp4 && tmp3 != tmp2 && tmp1 != tmp2 && tmp2 != 0 && tmp3 != 0)
                 {
+                    Console.WriteLine("hey15");
                     tmp3 = tmp3 * 2;
                     board.set_board_values(2, j, tmp3);
                     board.set_board_values(3, j, 0); //add function for block to appear
                     score.calculate_score(tmp3);
 
+                } else if(tmp1 != 0 && tmp2 != 0 && tmp3 == 0 && tmp4 != 0 && tmp1 != tmp2 && tmp2 != tmp4)
+                {
+                    Console.WriteLine("hey16");
+                    board.set_board_values(2, j, tmp4);
+                    board.set_board_values(3, j, 0);
+                }
+                else
+                {
+                    Console.WriteLine("hey17");
                 }
                 //else, then the player cannot move the grid this way
             }
@@ -356,8 +422,17 @@ namespace _2048_Final.Classes.Game
                     board.set_board_values(1, j, 0); //add function for block to appear
                     board.set_board_values(0, j, 0); //add function for block to appear
                     score.calculate_score(tmp2);
+                } 
+                else if(tmp3 == tmp4 && tmp3 != 0 && tmp2 != 0 && tmp2 != tmp3 && tmp1 == 0)
+                {
+                    tmp3 *= 2;
+                    board.set_board_values(3, j, tmp2);
+                    board.set_board_values(2, j, tmp3);
+                    board.set_board_values(1, j, 0);
+                    board.set_board_values(0, j, 0);
+                    score.calculate_score(tmp3);
                 }
-                else if (tmp3 == tmp4 && tmp1 == 0 && tmp2 == 0)
+                else if (tmp3 == tmp4 && tmp1 == 0 && tmp2 == 0 && tmp3 != 0)
                 {
                     tmp3 = tmp3 * 2;
                     board.set_board_values(3, j, tmp3);
@@ -374,6 +449,12 @@ namespace _2048_Final.Classes.Game
                     score.calculate_score(tmp3);
 
                 }
+                else if (tmp2 != 0 && tmp1 == 0 && tmp3 == 0 && tmp4 == 0)
+                {
+                    Console.Write("HEYYYYY");
+                    board.set_board_values(3, j, tmp2);
+                    board.set_board_values(2, j, 0);
+                }
                 else if (tmp1 == 0 && tmp2 == 0 && tmp3 == 0 && tmp4 != 0)
                 {
                     board.set_board_values(3, j, tmp4);
@@ -381,9 +462,9 @@ namespace _2048_Final.Classes.Game
                 }
                 else if (tmp1 != tmp3 && tmp2 == 0 && tmp1 != 0 && tmp3 != tmp4)
                 {
-                    board.set_board_values(3, j, tmp3);
-                    board.set_board_values(2, j, 0); //add function for block to appear
-                    board.set_board_values(1, j, 0); //add function for block to appear
+                    board.set_board_values(2, j, tmp3);
+                    board.set_board_values(1, j, tmp4); //add function for block to appear
+                    board.set_board_values(0, j, 0); //add function for block to appear
                 }
                 else if (tmp1 != tmp4 && tmp2 == 0 && tmp1 != 0 && tmp3 == 0 && tmp4 != 0)
                 {
@@ -394,18 +475,32 @@ namespace _2048_Final.Classes.Game
                 else if (tmp2 == tmp3 && tmp1 != tmp2 && tmp1 != 0 && tmp2 != 0 && tmp3 != 0)
                 {
                     tmp2 = tmp2 * 2;
-                    board.set_board_values(3, j, tmp2);
-                    board.set_board_values(2, j, tmp4);
+                    board.set_board_values(2, j, tmp2);
                     board.set_board_values(1, j, 0); //add function for block to appear
                     score.calculate_score(tmp2);
                 }
-                //else, then the player cannot move the grid this way
+                else if (tmp1 == 0 && tmp2 != 0 && tmp3 == 0 && tmp4 != 0 && tmp2 != tmp4)
+                {
+                    board.set_board_values(3, j, tmp2);
+                    board.set_board_values(2, j, tmp4);
+                    board.set_board_values(0, j, 0);
+                }
                 else if (tmp1 == 0 && tmp2 != 0)
                 {
                     board.set_board_values(3, j, tmp2);
                     board.set_board_values(2, j, tmp3);
                     board.set_board_values(1, j, tmp4);
                     board.set_board_values(0, j, 0); //add function for block to appear
+                }
+                else if(tmp4 != 0 && tmp1 == 0 && tmp2 == 0 && tmp3 == 0)
+                {
+                    board.set_board_values(3, j, tmp4);
+                    board.set_board_values(0, j, 0);
+                }
+                else if(tmp1 != 0 && tmp3 != 0 && tmp1 != tmp3 && tmp2 == 0 && tmp4 == 0)
+                {
+                    board.set_board_values(2, j, tmp3);
+                    board.set_board_values(1, j, 0);
                 }
                 else if (tmp1 == 0 && tmp2 == 0 && tmp3 != 0)
                 {
@@ -414,15 +509,15 @@ namespace _2048_Final.Classes.Game
                     board.set_board_values(1, j, 0);
                     board.set_board_values(0, j, 0); //add function for block to appear
                 }
+                else if(tmp1 != 0 && tmp2 == 0 && tmp3 == 0 && tmp4 == 0)
+                {
+                    break;
+                }
                 else if (tmp1 != tmp3 && tmp2 == 0)
                 {
                     board.set_board_values(1, j, tmp3);
                     board.set_board_values(2, j, tmp4);
                     board.set_board_values(3, j, 0); //add function for block to appear
-                }
-                else
-                {
-                    locked++;
                 }
 
             }
@@ -470,6 +565,13 @@ namespace _2048_Final.Classes.Game
                     board.set_board_values(i, 3, 0); //add function for block to appear
                     score.calculate_score(tmp1);
                 }
+                else if(tmp1 == tmp4 && tmp1 != 0 && tmp2 == 0 && tmp3 == 0)
+                {
+                    tmp1 *= 2;
+                    board.set_board_values(i, 0, tmp1);
+                    board.set_board_values(i, 3, 0);
+                    score.calculate_score(tmp1);
+                }
                 else if (tmp3 == tmp4 && tmp3 != tmp2 && tmp1 != tmp2 && tmp1 != 0 && tmp2 != 0 && tmp3 != 0)
                 {
                     tmp3 = tmp3 * 2;
@@ -503,6 +605,14 @@ namespace _2048_Final.Classes.Game
                     board.set_board_values(i, 2, 0); //add function for block to appear
                     board.set_board_values(i, 3, 0); //add function for block to appear
                     score.calculate_score(tmp2);
+                } else if(tmp3 == tmp4 && tmp3 != 0 && tmp1 == 0 && tmp2 != 0 && tmp2 != tmp3)
+                {
+                    tmp3 *= 2;
+                    board.set_board_values(i, 0, tmp2);
+                    board.set_board_values(i, 1, tmp3);
+                    board.set_board_values(i, 2, 0);
+                    board.set_board_values(i, 3, 0);
+                    score.calculate_score(tmp3);
                 }
                 else if (tmp3 == tmp4 && tmp1 == 0 && tmp2 == 0)
                 {
@@ -527,6 +637,10 @@ namespace _2048_Final.Classes.Game
                     board.set_board_values(i, 2, tmp4);
                     board.set_board_values(i, 3, 0); //add function for block to appear
                     score.calculate_score(tmp2);
+                }
+                else if(tmp1 != 0 && tmp4 != 0 && tmp2 == 0 && tmp3 == 0 && tmp1 != tmp4){
+                    board.set_board_values(i, 1, tmp4);
+                    board.set_board_values(i, 3, 0);
                 }
                 else if (tmp1 != tmp3 && tmp2 == 0 && tmp1 != 0 && tmp3 != tmp4)
                 {
@@ -713,6 +827,11 @@ namespace _2048_Final.Classes.Game
                     board.set_board_values(i, 1, tmp4);
                     board.set_board_values(i, 0, 0); //add function for block to appear
                     score.calculate_score(tmp2);
+                }
+                else if(tmp2 == 0 && tmp3 == 0 && tmp1 != 0 && tmp4 != 0 && tmp1 != tmp4)
+                {
+                    board.set_board_values(i, 2, tmp4);
+                    board.set_board_values(i, 0, 0);
                 }
                 else if (tmp1 != tmp3 && tmp2 == 0 && tmp1 != 0 && tmp3 != tmp4)
                 {
--- 2048-Final/Classes/Message.cs
+++ 2048-Final/Classes/Message.cs
@@ -7,45 +7,104 @@ using System.Threading.Tasks;
 namespace _2048_Final.Classes.Message
 {
     using Game;
+    using Board;
+    using Score;
     class Message
     {
         private bool _lost;
-        private string msg;
+        internal string msg;
         private string input;
 
         public Message()
         {
-            _lost = false;
-            msg = "nothing";
+            this.msg = "nothing";
         }
 
-        private void  display_msg(bool _lost, int score)
+        private void display_msg(int score)
         {
-            if (!_lost) {
-                Console.WriteLine("You won!");
-                Console.WriteLine("Score: " + score);
-                //print high score
-                while (input[0] != 'y' || input[0] != 'n' || input[0] != 'Y' || input[0] != 'Y')
+            Console.WriteLine(msg);
+            Console.WriteLine("Score: " + score);
+            //print high score
+            if (!_lost)
+            {
+                do
                 {
                     Console.WriteLine("Continue Y/N: ");
                     input = Console.ReadLine();
-                }
-                if(input[0] == 'n' || input[0] == 'N')
+                } while (input[0] != 'y' || input[0] != 'n' || input[0] != 'Y' || input[0] != 'Y');
+                if (input[0] == 'n' || input[0] == 'N')
                 {
                     Environment.Exit(0);
                 }
+            } else
+            {
+                Environment.Exit(0);
             }
         }
-
-        internal void lost()
+        internal void lost_check(Board board, int score) //will call this function after the keyboard press
         {
-
+            int check = 0;
+            check = check_vertical(board);
+            check += check_horizontal(board);
+            //check += 
+            if (check == 8)
+            {
+                msg = "You lost!";
+                display_msg(score);
+                _lost = true;
+            }
+            return;
         }
 
         internal void won(int block, int score)
         {
             if (block == 2048)
-                display_msg(_lost, score);
+            {
+                msg = "You won!";
+                display_msg(score);
+                _lost = false;
+            }
+        }
+
+        private int check_vertical(Board board)
+        {
+
+            int i = 0, j;
+            int check = 0; //if it equals 4, then they can't move this way
+            int tmp1, tmp2, tmp3, tmp4;
+
+            for (j = 0; j <= 3; j++)
+            {
+                tmp1 = board.get_board_values(i, j); //uppermost
+                tmp2 = board.get_board_values(i + 1, j); //2nd uppermost
+                tmp3 = board.get_board_values(i + 2, j); //3rd uppermost
+                tmp4 = board.get_board_values(i + 3, j); //4th uppermost
+
+                if (tmp1 != tmp2 && tmp2 != tmp3 && tmp3 != tmp4 && tmp1 != 0 && tmp2 != 0 && tmp3 != 0 && tmp4 != 0)
+                    check++;
+            }
+            return check;
+        }       
+        private int check_horizontal(Board board)
+        {
+
+            int i = 0, j;
+            int check = 0; //if it equals 4, then they can't move this way
+            int tmp1, tmp2, tmp3, tmp4;
+
+            for (j = 0; j <= 3; j++)
+            {
+                tmp1 = board.get_board_values(j, i); //leftmost
+                tmp2 = board.get_board_values(j, i + 1);
+                tmp3 = board.get_board_values(j, i + 2);
+                tmp4 = board.get_board_values(j, i + 3);
+
+                if (tmp1 != tmp2 && tmp2 != tmp3 && tmp3 != tmp4 && tmp1 != 0 && tmp2 != 0 && tmp3 != 0 && tmp4 != 0)
+                    check++;
+            }
+            return check;
         }
     }
+ 
 }
+
